2e59d639b3c12f3a788a4b59ecb5ebd04f3fc7f7
Carlos Osorio <hacybeyker@gmail.com>
2023-05-19 20:55:48 -0500

    migration

--- module/build.gradle.kts
+++ module/build.gradle.kts
@@ -1,28 +1,42 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("kotlin-kapt")
-    id("kotlin-parcelize")
-    id("maven-publish")
+    id("com.android.library") version "7.3.1"
+    id("org.jetbrains.kotlin.android") version "1.7.20"
+    id("org.jetbrains.kotlin.kapt") version "1.7.20"
+    id("com.google.dagger.hilt.android") version "2.44.2"
+    id("org.jetbrains.kotlin.plugin.parcelize") version "1.7.20"
+    id("org.sonarqube") version "3.3"
+    id("jacoco")
 }
 
-apply(from = "sonarqube.gradle")
-apply(from = "jacoco.gradle")
-apply(from = "upload.gradle")
+apply {
+    from("sonarqube.gradle")
+    from("jacoco.gradle")
+    from("upload.gradle")
+}
 
 android {
+    namespace = "com.afoxplus.restaurants"
     compileSdk = Versions.compileSdkVersion
-    buildToolsVersion = Versions.buildToolsVersion
 
     defaultConfig {
         minSdk = Versions.minSdkVersion
         targetSdk = Versions.targetSdkVersion
         testInstrumentationRunner = Versions.testInstrumentationRunner
         consumerProguardFiles("consumer-rules.pro")
+        renderscriptSupportModeEnabled = true
     }
 
     buildTypes {
         getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        create("staging") {
+            initWith(getByName("debug"))
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -52,33 +66,57 @@ android {
     }
 
     lint {
-        isCheckDependencies = true
+        disable.addAll(
+            listOf(
+                "TypographyFractions",
+                "TypographyQuotes",
+                "JvmStaticProvidesInObjectDetector",
+                "FieldSiteTargetOnQualifierAnnotation",
+                "ModuleCompanionObjects",
+                "ModuleCompanionObjectsNotInModuleParent"
+            )
+        )
+        checkDependencies = true
+        abortOnError = false
+        ignoreWarnings = false
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+
+    hilt {
+        enableAggregatingTask = true
     }
 }
 
 dependencies {
     implementation(fileTree("libs") { include(listOf("*.jar", "*.aar")) })
-    implementation(Deps.Jetpack.kotlin)
+
     implementation(Deps.Jetpack.core)
     implementation(Deps.Jetpack.appcompat)
+
     implementation(Deps.Jetpack.activity)
     implementation(Deps.Jetpack.fragment)
 
     implementation(Deps.UI.materialDesign)
     implementation(Deps.UI.constraintLayout)
-    implementation(Deps.UI.materialDesign)
-    implementation(Deps.UI.constraintLayout)
-    implementation(Deps.UI.uikit)
+
     implementation(Deps.UI.glide)
     kapt(Deps.UI.glideCompiler)
 
+    implementation(Deps.Arch.network)
+    implementation(Deps.Arch.retrofit2)
+    implementation(Deps.Arch.loggingInterceptor)
+    implementation(Deps.Arch.gson)
+
     implementation(Deps.Arch.coroutinesCore)
+
     implementation(Deps.Arch.hiltAndroid)
     kapt(Deps.Arch.hiltCompiler)
-    implementation(Deps.Arch.network)
-    api(Deps.Arch.retrofit2)
-    api(Deps.Arch.gson)
-    api(Deps.Arch.loggingInterceptor)
 
     testImplementation(Deps.Test.jUnit)
     androidTestImplementation(Deps.Test.androidJUnit)
@@ -87,4 +125,9 @@ dependencies {
     testImplementation(Deps.Test.mockitoInline)
     testImplementation(Deps.Test.testCore)
     testImplementation(Deps.Test.kotlinCoroutine)
+
+    //Business Dependencies
+    implementation(Deps.UI.uikit)
+
+    LocalModules.setupBuildGradle(this, rootProject, "restaurants")
 }

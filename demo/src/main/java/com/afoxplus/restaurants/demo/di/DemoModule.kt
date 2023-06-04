4055909f6c9e8d50c5383156195637ee7757d843
Valentin Mendoza Torres <jmendozat13@gmail.com>
2023-06-04 13:09:54 -0500

    Update with network module (#8)
    * Remove retrofit
    
    * Update version 3.0.2

---
+++ demo/src/main/java/com/afoxplus/restaurants/demo/di/DemoModule.kt
@@ -0,0 +1,16 @@
+package com.afoxplus.restaurants.demo.di
+
+import com.afoxplus.network.global.AppProperties
+import com.afoxplus.restaurants.demo.global.AppPropertiesDemo
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface DemoModule {
+
+    @Binds
+    fun bindAppProperties(demo: AppPropertiesDemo): AppProperties
+}
--- module/src/main/java/com/afoxplus/restaurants/repositories/sources/network/api/RestaurantApiNetwork.kt
+++ module/src/main/java/com/afoxplus/restaurants/repositories/sources/network/api/RestaurantApiNetwork.kt
@@ -1,9 +1,12 @@
 package com.afoxplus.restaurants.repositories.sources.network.api
 
+import com.afoxplus.network.annotations.ServiceClient
+import com.afoxplus.network.api.UrlProvider
 import com.afoxplus.network.response.BaseResponse
 import com.afoxplus.restaurants.repositories.sources.network.api.response.RestaurantResponse
 import retrofit2.http.GET
 
+@ServiceClient(type = UrlProvider.Type.API_RESTAURANTS)
 internal interface RestaurantApiNetwork {
 
     companion object {

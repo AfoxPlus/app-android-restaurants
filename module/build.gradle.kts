61cd2e8d260107f89b235876157e0f63c1c780d3
Valentin Mendoza Torres <jmendozat13@gmail.com>
2024-03-19 15:44:17 -0500

    RC03.2023 (#18)
    * changes
    
    * changes
    
    * changes
    
    ---------
    
    Co-authored-by: Carlos Osorio <hacybeyker@gmail.com>

--- module/build.gradle.kts
+++ module/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.library") version "7.3.1"
-    id("org.jetbrains.kotlin.android") version "1.7.20"
-    id("org.jetbrains.kotlin.kapt") version "1.7.20"
+    id("com.android.library") version "8.0.2"
+    id("org.jetbrains.kotlin.android") version "1.8.22"
+    id("org.jetbrains.kotlin.kapt") version "1.8.22"
     id("com.google.dagger.hilt.android") version "2.44.2"
     id("org.jetbrains.kotlin.plugin.parcelize") version "1.7.20"
     id("org.sonarqube") version "3.3"
@@ -12,6 +12,7 @@ apply {
     from("sonarqube.gradle")
     from("jacoco.gradle")
     from("upload.gradle")
+    from("graph.gradle.kts")
 }
 
 android {
@@ -54,15 +55,20 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
 
     buildFeatures {
         viewBinding = true
         dataBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.8"
     }
 
     lint {
@@ -95,29 +101,35 @@ android {
 
 dependencies {
     implementation(fileTree("libs") { include(listOf("*.jar", "*.aar")) })
-
+    implementation(Deps.Jetpack.kotlin)
     implementation(Deps.Jetpack.core)
     implementation(Deps.Jetpack.appcompat)
-
-    implementation(Deps.Jetpack.activity)
     implementation(Deps.Jetpack.fragment)
-
     implementation(Deps.UI.materialDesign)
     implementation(Deps.UI.constraintLayout)
 
-    implementation(Deps.UI.glide)
-    kapt(Deps.UI.glideCompiler)
+    // JetpackCompose
+    implementation(Deps.JetpackCompose.activity)
+    implementation(Deps.JetpackCompose.constraintlayout)
+    implementation(Deps.JetpackCompose.navigation)
+    implementation(platform(Deps.JetpackCompose.bom))
+    implementation(Deps.JetpackCompose.ui)
+    implementation(Deps.JetpackCompose.graphics)
+    implementation(Deps.JetpackCompose.toolingPreview)
+    implementation(Deps.JetpackCompose.material3)
+    implementation(Deps.JetpackCompose.materialIconExtended)
 
-    implementation(Deps.Arch.network)
+    // External Libraries
+    implementation(Deps.Arch.hiltAndroid)
+    kapt(Deps.Arch.hiltCompiler)
+    implementation(Deps.Arch.coroutinesCore)
     implementation(Deps.Arch.retrofit2)
     implementation(Deps.Arch.loggingInterceptor)
     implementation(Deps.Arch.gson)
+    implementation(Deps.UI.glide)
+    kapt(Deps.UI.glideCompiler)
 
-    implementation(Deps.Arch.coroutinesCore)
-
-    implementation(Deps.Arch.hiltAndroid)
-    kapt(Deps.Arch.hiltCompiler)
-
+    // Test
     testImplementation(Deps.Test.jUnit)
     androidTestImplementation(Deps.Test.androidJUnit)
     androidTestImplementation(Deps.Test.espresso)
@@ -128,6 +140,5 @@ dependencies {
 
     //Business Dependencies
     implementation(Deps.UI.uikit)
-
-    LocalModules.setupBuildGradle(this, rootProject, "restaurants")
+    implementation(Deps.Arch.network)
 }
